889f93455a3a7746b8defe7ae8d19b8abe268e5e
Johnny Austen <johnnydausten@gmail.com>
2021-03-25 16:29:51 +0000

    Build JavaScript Components for Modal

---
+++ js/projects.js
@@ -0,0 +1,14 @@
+const data = [
+  { id: 0, title: "Web Design - Grand Technical", image: "" },
+  {
+    id: 1,
+    title: "Full Stack Courses App",
+    image: "/img/fullstack-courses-homepage-1200.jpg",
+    description:
+      "In this project, I have used the popular Javascript library React to create a client for a school database REST API (that I created in a previous project). This project allows users to view a list of all of the courses held in the API, and view all of the details for each course. Users may also register an account, or sign in with an existing account, to create, update and delete existing courses. I have also implemented a number of validation methods to ensure that users cannot update or delete projects that do not belong to them.",
+    live: "https://full-stack-courses-app.herokuapp.com/",
+    github: "https://github.com/jmaletto1/JS-TD-P10-Full-Stack-React-App",
+  },
+  { id: 2, title: "Web App Traffic Dashboard", image: "" },
+  { id: 3, title: "Startup Employee Directory", image: "" },
+];
--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
+// import data from "projects";
+
 const sideNav = document.querySelector("#phone--sidenav");
 const sideNavLink = document.querySelectorAll(".sidenav--link");
 const mainDiv = document.querySelector(".main");
 const mobileHamburger = document.querySelector(".mobile--hamburger");
+const modalContainer = document.querySelector(".modal-window__container");
+const modalXButton = document.querySelector(".modal--button");
+const modalContent = document.querySelector(".modal--project");
+const projectButtons = document.querySelectorAll(".project--button");
 
+// Open Mobile Nav Function
 function openNav() {
   sideNav.style.width = "200px";
   mainDiv.style.marginLeft = "200px";
@@ -10,6 +17,7 @@ function openNav() {
   mobileHamburger.style.visibility = "hidden";
 }
 
+// Close Mobile Nav Function
 function closeNav() {
   sideNav.style.width = "0px";
   mainDiv.style.marginLeft = "0";
@@ -17,8 +25,52 @@ function closeNav() {
   mobileHamburger.style.visibility = "visible";
 }
 
+// Add Event Listeners to close Mobile Nav when a link is clicked.
 sideNavLink.forEach((link) => {
   link.addEventListener("click", () => {
     closeNav();
   });
 });
+
+// Close Modal Window when button is clicked
+modalXButton.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+  modalContent.innerHTML = "";
+});
+
+// Additionally, close the window if the user clicks outside of the modal window
+window.onclick = function (e) {
+  if (e.target == modalContainer) {
+    modalContainer.style.display = "none";
+    modalContent.innerHTML = "";
+  }
+};
+
+// Open Modal Listeners
+
+for (let button = 0; button < projectButtons.length; button++) {
+  projectButtons[button].id = button;
+}
+
+projectButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    createModal(e.target.id);
+  });
+});
+
+// Create Modal Function
+
+const createModal = (id) => {
+  const project = data[id];
+  modalContainer.style.display = "block";
+  const content = `
+  <h2 class="modal--h2">${project.title}</h2>
+  <img src=${project.image}>
+  <p>${project.description}</p>
+  <div class="project__links">
+    <a href="${project.live}" target="_blank" class="live--button bouncy">LIVE LINK</a>
+    <a href=${project.github} target="_blank" class="live--button bouncy" style="animation-delay:0.07s">GITHUB REPO</a>
+  </div>
+  `;
+  modalContent.insertAdjacentHTML("beforeend", content);
+};
